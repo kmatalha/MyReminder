1f98f5258eb87d228134f947c4e2106ba577f2a8
kmatalha <80261405+kmatalha@users.noreply.github.com>
2026-07-21 15:22:37 +0600

    Add files via upload

--- alarm.py
+++ alarm.py
@@ -6,6 +6,7 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtCore import Qt, QTimer, QObject
 from sound import play_alarm_sound, stop_alarm_sound
 
+
 class AlarmPopup(QDialog):
     def __init__(self, tasks, parent=None):
         super().__init__(parent)
@@ -112,4 +113,111 @@ class AlarmPopup(QDialog):
 
         self.adjustSize()
 
-    # ... (rest of AlarmPopup methods unchanged)
+    def remove_task(self, task_id):
+        if task_id in self.task_frames:
+            frame, _ = self.task_frames.pop(task_id)
+            self.content_layout.removeWidget(frame)
+            frame.deleteLater()
+        if not self.task_frames:
+            self.close_ok()
+
+    def handle_paid(self, task_id):
+        self.parent_app.db.mark_paid(task_id)
+        self.parent_app.alarm_active.discard(task_id)
+        self.remove_task(task_id)
+
+    def handle_snooze_today(self, task_id):
+        end = datetime.datetime.now().replace(hour=23, minute=59, second=59, microsecond=0)
+        self.parent_app.db.set_snooze(task_id, end)
+        self.parent_app.alarm_active.discard(task_id)
+        self.remove_task(task_id)
+
+    def handle_snooze(self, task_id, minutes=5):
+        until = datetime.datetime.now() + datetime.timedelta(minutes=minutes)
+        self.parent_app.db.set_snooze(task_id, until)
+        self.parent_app.alarm_active.discard(task_id)
+        self.remove_task(task_id)
+
+    def handle_dismiss(self, task_id):
+        self.handle_snooze_today(task_id)
+
+    def handle_dismiss_all(self):
+        now = datetime.datetime.now()
+        end = now.replace(hour=23, minute=59, second=59, microsecond=0)
+        for tid in list(self.task_frames.keys()):
+            self.parent_app.db.set_snooze(tid, end)
+            self.parent_app.alarm_active.discard(tid)
+            self.remove_task(tid)
+        self.close_ok()
+
+    def close_ok(self):
+        stop_alarm_sound()
+        self.parent_app.alarm_popup = None
+        self.parent_app.refresh_dashboard()
+        self.close()
+
+    def closeEvent(self, event):
+        self.handle_dismiss_all()
+        event.accept()
+
+
+class AlarmChecker(QObject):
+    def __init__(self, db, main_window):
+        super().__init__()
+        self.db = db
+        self.main_window = main_window
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.check)
+        self.timer.start(10000)
+
+    def check(self):
+        if self.main_window.alarm_popup is not None:
+            return
+        now = datetime.datetime.now()
+        today = now.day
+        current_ym = now.strftime("%Y-%m")
+        tasks = self.db.get_all_tasks()
+        triggered = []
+        for t in tasks:
+            tid, _, _, due_day, _, snooze, start_before, alarm_time, interval, current_due = t
+            if not current_due or current_due == "9999-12":
+                continue
+            if current_ym < current_due:
+                continue
+
+            expired_snooze = False
+            if snooze:
+                try:
+                    snooze_dt = datetime.datetime.strptime(snooze, "%Y-%m-%d %H:%M:%S")
+                    if now < snooze_dt:
+                        continue
+                    else:
+                        expired_snooze = True
+                except:
+                    pass
+
+            if not expired_snooze:
+                try:
+                    ah, am = map(int, alarm_time.split(":"))
+                    if not (0 <= ah < 24 and 0 <= am < 60):
+                        raise ValueError
+                except:
+                    ah, am = 9, 0
+                if now.hour != ah or now.minute != am:
+                    continue
+
+            start_day = max(1, due_day - start_before)
+            if today < start_day or today > due_day:
+                continue
+
+            if tid in self.main_window.alarm_active:
+                continue
+
+            if snooze:
+                self.db.clear_snooze(tid)
+
+            self.main_window.alarm_active.add(tid)
+            triggered.append(t)
+
+        if triggered:
+            self.main_window.show_alarm(triggered)
